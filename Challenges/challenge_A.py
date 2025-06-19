3b97dc2b68d32497359df1282a539c9314b62b49
Tahlia11 <tahlia.wenger834@education.nsw.gov.au>
2025-06-19 13:06:06 +1000

    Update challenge_A.py

--- Challenges/challenge_A.py
+++ Challenges/challenge_A.py
@@ -1,4 +1,4 @@
-#exercise 1
+#Exercise 1
 name = str(input("What is your childs name?"))
 age = int(input("What is their age?"))
 snack = str(input("What snack do they want to bring?"))
@@ -6,7 +6,7 @@ game_time = float(input("how long should they be in the game? Answer in minutes"
 water_play = bool(input("Do they like water play? Answer yes/no"))
 print(f"Hi {name}! You're {age} years old and are ready to play in Bluey's backyard. You'll play for {game_time} minutes and bring your snack: {snack}. Water play? {water_play}!")
 
-#exercise 2
+#Exercise 2
 games_list = ["Keepy Uppy", "Magic Asparagus", "Shadowlands", "Obstactle course", "Muffin Cone"]
 print(games_list)
 
