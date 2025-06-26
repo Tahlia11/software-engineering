b62cb87dd51d6ed22ea7d799a98d58fa8d9d8bbe
Tahlia11 <tahlia.wenger834@education.nsw.gov.au>
2025-06-26 15:04:04 +1000

    26/6/25 my death date

--- Challenges/challenge_B.py
+++ Challenges/challenge_B.py
@@ -1,13 +1,17 @@
 name = input(str("What is your username?"))
 woodBlocks = int(input("How many woodblocks do you have?"))
-woodWeight = float(input("What is the weight of a single woodblock?"))
-daytime = input("Is it daytime?").lower() == "yes"
+woodWeight = float(input("What is the weight of a single woodblock (kg)?"))
+daytime = input("Is it daytime? (yes/no) ").lower() == "yes"
+successful = 10
 
 while woodBlocks < 10:
-    print("How many more woodblocks would you like to collect?")
-    successfu = 10
-    for woodBlocks in range (1,10):
-        woodBlocks + 1
-        print(woodBlocks)
-        if successful:
-            break
+    print(f"You have {woodBlocks} blocks.") 
+    print(f"You need {10 - woodBlocks} more.")
+    how_many = int(input("How may more woodblocks would you like?"))
+    woodBlocks = how_many + woodBlocks
+
+print("Woodblock collection complete!")
+print(f"Username: {name}")
+print(f"Total Blocks: {woodBlocks}")
+print(f"Total weight: {woodWeight*woodBlocks} kg")
+print(f"It is daytime: {daytime}")
