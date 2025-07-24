1ab0c025fa538f89bcd584ac3c87606ff0335871
Tahlia11 <tahlia.wenger834@education.nsw.gov.au>
2025-07-24 15:04:53 +1000

    24/7/2025 What a time waster

--- pracs/prac8.py
+++ pracs/prac8.py
@@ -5,5 +5,9 @@ result = add_numbers(9,10)
 print(result)
 
 
-def minus_numbers():
-    """"""
+def minus_numbers(a,b):
+    """takes away one number from the other"""
+    return a-b
+
+result = minus_numbers(10,2)
+print(result)
