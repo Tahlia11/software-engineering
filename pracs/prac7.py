6fb93ce95cbf5d3d47ced52997c537c593d68d78
Tahlia11 <tahlia.wenger834@education.nsw.gov.au>
2025-07-31 09:10:11 +1000

    at home work

--- pracs/prac7.py
+++ pracs/prac7.py
@@ -1,8 +1,28 @@
 myNumbers = []
-for i in range (4):
+for i in range (1,4):
     userInput = (input("Enter number:"))
     myNumbers.append(userInput)
-    break 
+
 print(myNumbers)
 for numbers in myNumbers:
     print(numbers)
+
+
+for i in (1,6):
+    userInput = input("Enter a new value:")
+    myNumbers.append(userInput)
+
+sortedAscending = []
+sortedAscending = sorted(myNumbers)
+
+sortedDecending = []
+sortedDecending = sorted(myNumbers, reverse=True)
+
+print("Middle number of the original list:")
+print(myNumbers[2])
+
+print("Middle number of the sorted ascending list:")
+print(sortedAscending[2])
+
+print("Middle number of sorted decending list:")
+print(sortedDecending[2])
