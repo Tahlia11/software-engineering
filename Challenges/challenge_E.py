7d2d0eb291f93a7246555cd608838eebe24624c2
Tahlia11 <tahlia.wenger834@education.nsw.gov.au>
2025-08-01 14:07:26 +1000

    challenge e update 1/8/25

--- Challenges/challenge_E.py
+++ Challenges/challenge_E.py
@@ -10,13 +10,15 @@ print(calculator(6, 4, "add"))
 print(calculator(9, 7, "subtract"))
 print(calculator(10, 5, "divide"))
 
-
 def compare_numbers(num1, num2):
     if num1 > num2:
-        return 
+        return "Number 1 is greater"
     elif num1 < num2:
-        return
+        return "Number 2 is greater"
     else:
-        return
-    
-print(compare_numbers(3,4))
+        return "The numbers are equal"
+        
+result = compare_numbers(3,4)
+print(result)
+
+
