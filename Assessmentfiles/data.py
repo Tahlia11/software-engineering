36053a4846b96fdf8eb55e59a39f82eed3a49cf9
Tahlia11 <tahlia.wenger834@education.nsw.gov.au>
2025-08-05 15:05:09 +1000

    5/8/25

---
+++ Assessmentfiles/data.py
@@ -0,0 +1,15 @@
+intro = {
+        'breakspace': '______________________________________________________',
+        'welcome': ' Welcome to (gamename)',
+        'description': '',
+        'options': '',
+        'continue': 'To solve your case please type "Im ready"'
+}
+
+
+start=  {
+        'success': "SOME QUICK TIPS:",
+        'basic': '- All answers are case sensitive so make sure you copy comands exactly!\n - Type commands to view all your options\n' 
+    }
+print(start['basic'])
+
